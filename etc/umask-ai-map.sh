2f2f89511a8c9e01b028384b9866d862aacb04a1
Rob Beffers <rob.beffers@rivm.nl>
2019-12-14 17:19:57 +0100

    c

--- etc/umask-ai-map.sh
+++ etc/umask-ai-map.sh
@@ -1,7 +1,7 @@
 if [ -d "/ai-map" ]
 then
     umask 0002
-    exec lighttpd -D -f /etc/lighttpd.conf
+	while true; do sleep 5 ; done
 else
     echo "Voeg persistent volume /ai-map toe om de build af te kunnen ronden."
     exit 1
