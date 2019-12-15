7ffe48aa2daddd897f884199cf6d7f0c4a8325f9
Rob Beffers <rob.beffers@rivm.nl>
2019-12-15 19:07:44 +0100

    c

--- ai_project/urls.py
+++ ai_project/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^ai_app/', include('/ai_app.urls')),
+    url(r'^ai_app/', ai_app.site.urls),
     url(r'^admin/', admin.site.urls),
 ]
